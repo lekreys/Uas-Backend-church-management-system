ae8935099c4364f4a97c6f645f05d13fa087b2af
Sabiicuu <152463472+Sabiicuu@users.noreply.github.com>
2024-06-27 19:52:11 +0700

    Add files via upload

---
+++ database/migrations/2024_06_18_073811_create_anak.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('anak', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string('tanggal_lahir');
+            $table->string('nama_ibu');
+            $table->integer('umur'); 
+            $table->enum('wilayah', ['1', '2', '3', '4']);
+            $table->enum("jabatan" , ['AS' ,'ASM' , 'GSM']);
+            $table->string("alamat");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('anak');
+    }
+};
